1c4ca1b33f66eb3235cca8ce943627a5c3eee840
aphelionxd <r.batista@aluno.ifsp.edu.br>
2025-04-19 15:39:23 -0300

    add planos e valores in site

--- scripts.js
+++ scripts.js
@@ -1,14 +1,20 @@
-const elements = document.querySelectorAll('.fade-in');
+const nav = document.querySelector('.nav-discreta');
 
-function revealOnScroll() {
-  const windowHeight = window.innerHeight;
-  elements.forEach(el => {
-    const elementTop = el.getBoundingClientRect().top;
-    if (elementTop < windowHeight - 100) {
-      el.classList.add('visible');
+// Configuração do IntersectionObserver para monitorar quando as seções estão visíveis
+const observer = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      // Se a seção estiver visível, o menu não ficará sobrepondo
+      nav.classList.remove('sobrepondo');
+    } else {
+      // Se a seção não estiver visível, o menu ficará sobrepondo
+      nav.classList.add('sobrepondo');
     }
   });
-}
+}, { threshold: 0.1 }); // A 10% de visibilidade, o menu começa a mudar
 
-window.addEventListener('scroll', revealOnScroll);
-window.addEventListener('load', revealOnScroll);
+// Observando todas as seções
+document.querySelectorAll('section').forEach(sec => observer.observe(sec));
+
+// A função para voltar ao topo da página ao recarregar
+window.onbeforeunload = () => window.scrollTo(0, 0);
